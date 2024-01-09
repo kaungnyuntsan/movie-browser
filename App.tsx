a367e23d0fba823270ec04d0d669d40f8d3e64ec
Kaung-Nyunt-San <71218665+Kaung-Nyunt-San@users.noreply.github.com>
2024-01-09 23:24:11 +0630

    react nav typescript test

--- App.tsx
+++ App.tsx
@@ -1,14 +1,55 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from "@react-navigation/native-stack";
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, "Details">;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 20 }}>
+        Open up App.tsx to start working on your app!!!
+      </Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+const DetailsScreen = ({ navigation }: DetailsScreenProps) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +59,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default App;
